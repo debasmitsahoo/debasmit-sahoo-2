057cc685dc6f82d182b58cb50029249d1e62272d
Debasmit Sahoo <sahoodebasmit@gmail.com>
2024-08-11 22:40:23 +0530

    Update

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinlight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
-import CV from "../../assets/cv.pdf";
+import CV from "../../assets/CV.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
